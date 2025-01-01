5c277281a6734672bb3993f409dd7fb1cf625a3a
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-01-01 19:32:51 +0700

    Update proxmox-non-subscription.sh

--- proxmox-non-subscription.sh
+++ proxmox-non-subscription.sh
@@ -8,36 +8,35 @@
 again='y'
 while [[ $again == 'Y' ]] || [[ $again == 'y' ]] ; do
 clear
-echo "                                                                     			";
-echo "												";
-echo "  ██████╗ ██████╗  ██████╗ ██╗  ██╗███╗   ███╗ ██████╗ ██╗  ██╗			";
-echo "  ██╔══██╗██╔══██╗██╔═══██╗╚██╗██╔╝████╗ ████║██╔═══██╗╚██╗██╔╝		";
-echo "  ██████╔╝██████╔╝██║   ██║ ╚███╔╝ ██╔████╔██║██║   ██║ ╚███╔╝ 		";
-echo "  ██╔═══╝ ██╔══██╗██║   ██║ ██╔██╗ ██║╚██╔╝██║██║   ██║ ██╔██╗			"; 
-echo "  ██║     ██║  ██║╚██████╔╝██╔╝ ██╗██║ ╚═╝ ██║╚██████╔╝██╔╝ ██╗			";
-echo "  ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝			";
-echo "                                                                     			";                                                         
-echo "     _  __            ____     __              _      __  _        	";
-echo "    / |/ /__  ___    / __/_ __/ /  ___________(_)__  / /_(_)__  ___ 	";
-echo "   /    / _ \/ _ \  _\ \/ // / _ \(_-< __/ __/ / _ \/ __/ / _ \/ _ \	";
-echo "  /_/|_/\___/_//_/ /___/\_,_/_.__/___|__/_/ /_/ .__/\__/_/\___/_//_/	";
-echo "                                           /_/                    	";
-echo "								";
-echo "   Version: 1.0 - 01/01/2025                            	";
-echo "   Developer: https://github.com/anggrdwjy              	";
-echo "   Support OS : Proxmox 7.4 - Proxmox 8.3                	";
-echo "                                                        	";
-echo "   __________________________________________________   	";                                                            
-echo "                                                  	";
-echo "   Options List :                                 	";
+echo "                                                                       ";
+echo "        ____                                                           ";
+echo "       / __ \_________  _  __   ____ ___  ____  _  __                  ";
+echo "      / /_/ / ___/ __ \| |/_/  / __ `__ \/ __ \| |/_/                  ";
+echo "     / ____/ /  / /_/ />  <   / / / / / / /_/ />  <                    ";
+echo "    /_/   /_/   \____/_/|_|  /_/ /_/ /_/\____/_/|_|                    ";                          
+echo "												                                   ";
+echo "        _   __               _____       __                   _       __  _              ";
+echo "       / | / /___  ____     / ___/__  __/ /_  _______________(_)___  / /_(_)___  ____    ";
+echo "      /  |/ / __ \/ __ \    \__ \/ / / / __ \/ ___/ ___/ ___/ / __ \/ __/ / __ \/ __ \   ";
+echo "     / /|  / /_/ / / / /   ___/ / /_/ / /_/ (__  ) /__/ /  / / /_/ / /_/ / /_/ / / / /   ";
+echo "    /_/ |_/\____/_/ /_/   /____/\__,_/_.___/____/\___/_/  /_/ .___/\__/_/\____/_/ /_/    ";
+echo "                                                         /_/                             ";           
+echo "												                                 ";
+echo "   Version: 1.0 - 01/01/2025                            	            ";
+echo "   Developer: https://github.com/anggrdwjy              	            ";
+echo "   Support OS : Proxmox 7.4 - Proxmox 8.3                	         ";
+echo "                                                        	            ";
+echo "   __________________________________________________   	            ";                                                            
+echo "                                                  	         ";
+echo "   Options List :                                 	         ";
 echo "   1) Add Repository Proxmox 8.3 Non-Subscription        	";
 echo "   2) Add Repository Proxmox 7.4 Non-Subscription        	";
-echo "   3) Reboot Server                              		";
-echo "   4) Exit                                       		";
-echo "   __________________________________________________   	";
-echo "                                                        	";
+echo "   3) Reboot Server                              		      ";
+echo "   4) Exit                                       		      ";
+echo "   __________________________________________________   	   ";
+echo "                                                           	";
 read -p "   Enter a number the options listed: " choice;
-echo "                                                        	";
+echo "                                                        	   ";
 case $choice in                       
 
 1) read -p "   Add Repository Proxmox 8.3 Non-Subcription and Update? y/n :" -n 1 -r
@@ -45,11 +44,11 @@ case $choice in
    echo "                                                  ";
    if [[ ! $REPLY =~ ^[Nn]$ ]] 
    then
-   sudo cp /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.bak
-   sudo cp /etc/apt/sources.list.d/ceph.list /etc/apt/sources.list.d/ceph.list.bak
-   sudo cp support-8.3/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list
-   sudo cp support-8.3/ceph.list /etc/apt/sources.list.d/ceph.list
-   sudo apt-get update
+   cp /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.bak
+   cp /etc/apt/sources.list.d/ceph.list /etc/apt/sources.list.d/ceph.list.bak
+   cp support-8.3/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list
+   cp support-8.3/ceph.list /etc/apt/sources.list.d/ceph.list
+   apt-get update
    echo "                                                  ";
    echo "   ======== Proxmox 8.3 Non-Subscription dan Update Done ========";
    echo "                                                  ";
@@ -61,11 +60,11 @@ case $choice in
    echo "                                                  ";
    if [[ ! $REPLY =~ ^[Nn]$ ]] 
    then
-   sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-   sudo cp /etc/apt/sources.list.d/ceph.list /etc/apt/sources.list.d/ceph.list.bak
-   sudo cp support-7.4/sources.list /etc/apt/sources.list
-   sudo cp support-7.4/ceph.list /etc/apt/sources.list.d/ceph.list
-   sudo apt-get update
+   cp /etc/apt/sources.list /etc/apt/sources.list.bak
+   cp /etc/apt/sources.list.d/ceph.list /etc/apt/sources.list.d/ceph.list.bak
+   cp support-7.4/sources.list /etc/apt/sources.list
+   cp support-7.4/ceph.list /etc/apt/sources.list.d/ceph.list
+   apt-get update
    echo "                                                  ";
    echo "   ======== Proxmox 8.3 Non-Subscription dan Update Done ========";
    echo "                                                  ";
