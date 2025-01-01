746c750ac92701728af38e60dc2bfe90d584cb8c
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-01-01 19:45:00 +0700

    Update proxmox-non-subscription.sh

--- proxmox-non-subscription.sh
+++ proxmox-non-subscription.sh
@@ -66,7 +66,7 @@ case $choice in
    cp support-7.4/ceph.list /etc/apt/sources.list.d/ceph.list
    apt-get update
    echo "                                                  ";
-   echo "   ======== Proxmox 8.3 Non-Subscription dan Update Done ========";
+   echo "   ======== Proxmox 7.4 Non-Subscription dan Update Done ========";
    echo "                                                  ";
    fi
    ;;
